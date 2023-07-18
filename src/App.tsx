c777604bf6ab01c214665bda6bf579cb1658c5e9
Sean <sdoesimkins@gmail.com>
2023-07-18 18:14:15 -0500

    Remove boilerplate and tweak some App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Table from "./components/Table/Table";
+import purchases from "./data/purchases.json";
 
 function App() {
+  // TODO HTTP requests from localhost have CORS issues from server so we have to hardcode data
+  // but in prod we'd want fresh data from an API
+  // try {
+  //   fetch( "https://idme.s3.amazonaws.com/interview/data.json");
+  // } catch (error) {
+  //   console.log(error);
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='wrapper'>
+        <Table data={purchases} />
+      </div>
     </div>
   );
 }
